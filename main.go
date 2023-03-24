f0b95c0a3be44c3550954e38351f5d9787345a1e
Thitisak Suthisoontrin <thitisakfw@gmail.com>
2023-03-24 13:32:21 +0700

    create delete get api

--- main.go
+++ main.go
@@ -1,7 +1,60 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Jackkeng")
+	r := gin.New()
+
+	r.GET("/books", listBooksHandler)
+	r.POST("/books", createBookHandler)
+	r.DELETE("/books/:id", deleteBookHandler)
+
+	r.Run()
+}
+
+type Book struct {
+	ID       string `json:"id"`
+	Username string `json:"username"`
+}
+
+var books = []Book{
+	{ID: "1", Username: "Professional1"},
+	{ID: "2", Username: "Professional2"},
+	{ID: "3", Username: "Professional3"},
+}
+
+func listBooksHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, books)
+}
+
+func createBookHandler(c *gin.Context) {
+	var book Book
+
+	if err := c.ShouldBindJSON(&book); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	books = append(books, book)
+
+	c.JSON(http.StatusCreated, book)
+}
+
+func deleteBookHandler(c *gin.Context) {
+	id := c.Param("id")
+
+	for i, a := range books {
+		if a.ID == id {
+			books = append(books[:i], books[i+1:]...)
+			break
+		}
+	}
+
+	c.Status(http.StatusNoContent)
 }
