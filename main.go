321c93b6d0ad66bd021cf5d920ed873b4344ca11
Thitisak Suthisoontrin <thitisakfw@gmail.com>
2023-03-28 16:07:43 +0700

    degree profile endpoint

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"CRUD-API/handlers/position"
 	"CRUD-API/handlers/user"
+	"CRUD-API/handlers/degree"
+	"CRUD-API/handlers/profile"
 	// . "CRUD-API/models"
 
 	"github.com/gin-gonic/gin"
@@ -18,11 +21,38 @@ func main() {
 	}
 	r := gin.New()
 
+	//User Zones
 	userHandler := user.NewUserHandler(db)
 	r.GET("/user", userHandler.ListUser)
+	r.GET("/user/:id", userHandler.GetUserHandler)
 	r.POST("/user", userHandler.CreateUserHandler)
-	r.DELETE("/user/:id", userHandler.DeleteUserHandler)
 	r.PUT("/user/:id", userHandler.UpdateUserHandler)
+	r.DELETE("/user/:id", userHandler.DeleteUserHandler)
+
+	//Position Zones
+	positionHandler := position.NewPositionHandler(db)
+	r.GET("/position", positionHandler.ListPosition)
+	r.GET("/position/:id", positionHandler.GetPositionHandler)
+	r.POST("/position", positionHandler.CreatePositionHandler)
+	r.PUT("/position/:id", positionHandler.UpdatePositionHandler)
+	r.DELETE("/position/:id", positionHandler.DeletePositionHandler)
+
+	//Degree Zones
+	degreeHandler := degree.NewDegreeHandler(db)
+	r.GET("degree/", degreeHandler.ListDegree)
+	r.GET("/degree/:id", degreeHandler.GetDegreeHandler)
+	r.POST("/degree", degreeHandler.CreateDegreeHandler)
+	r.PUT("/degree/:id", degreeHandler.UpdateDegreeHandler)
+	r.DELETE("/degree/:id", degreeHandler.DeleteDegreeHandler)
+
+	//Profile Zones
+	profileHandler := profile.NewProfileHandler(db)
+	r.GET("profile/", profileHandler.ListProfile)
+	r.GET("/profile/:id", profileHandler.GetProfileHandler)
+	r.POST("/profile", profileHandler.CreateProfileHandler)
+	r.PUT("/profile/:id", profileHandler.UpdateProfileHandler)
+	r.DELETE("/profile/:id", profileHandler.DeleteProfileHandler)
+
 
 	r.Run()
 }
--- models/degree.go
+++ models/degree.go
@@ -1,13 +1,19 @@
 package models
 
-type degrees struct {
-	Id                int    `json:"id"`
+import "time"
+
+type Degree struct {
+	ID                int    `json:"id"`
 	Degree_type       string `json:"degree_type"`
 	Degree_program    string `json:"degree_program"`
 	Degree_university string `json:"degree_university"`
-	Profile_id        int    `json:"profile_id"`
-	Created_by        string `json:"created_by"`
-	Updated_by        string `json:"updated_by"`
-	Created_at        string `json:"created_at"`
-	Updated_at        string `json:"updated_at"`
+	Profile_id        int       `json:"profile_id"`
+	Created_by        string    `json:"created_by"`
+	Updated_by        string    `json:"updated_by"`
+	CreatedAt         time.Time `json:"created_at" gorm:"type:timestamp;default:CURRENT_TIMESTAMP"`
+	UpdatedAt         time.Time `json:"updated_at" gorm:"type:timestamp;default:CURRENT_TIMESTAMP"`
+}
+
+func (p *Degree) TableName() string {
+	return "degree"
 }
