8de110bd19caa15e888ada7844342488bb1a0ad6
Thitisak Suthisoontrin <thitisakfw@gmail.com>
2023-03-28 17:22:24 +0700

    degree get

--- models/program.go
+++ models/program.go
@@ -1,11 +1,17 @@
 package models
 
+import "time"
+
 type Program struct {
-	Id           int    `json:"id"`
-	Program_name string `json:"program_name"`
-	Profile_id   int    `json:"profile_id"`
-	Created_by   string `json:"created_by"`
-	Updated_by   string `json:"updated_by"`
-	Created_at   string `json:"created_at"`
-	Updated_at   string `json:"updated_at"`
+	Id           int       `json:"id"`
+	Program_name string    `json:"program_name"`
+	Profile_id   int       `json:"profile_id"`
+	Created_by   string    `json:"created_by"`
+	Updated_by   string    `json:"updated_by"`
+	CreatedAt    time.Time `json:"created_at" gorm:"type:timestamp;default:CURRENT_TIMESTAMP"`
+	UpdatedAt    time.Time `json:"updated_at" gorm:"type:timestamp;default:CURRENT_TIMESTAMP"`
+}
+
+func (p *Program) TableName() string {
+	return "program"
 }
