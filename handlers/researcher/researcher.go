d010c2625e84c91750091f83c39805c15064d01a
Thitisak Suthisoontrin <thitisakfw@gmail.com>
2023-03-31 12:09:04 +0700

    refactor researcher

--- handlers/researcher/researcher.go
+++ handlers/researcher/researcher.go
@@ -2,7 +2,7 @@ package researcher
 
 import (
 	"net/http"
-
+	"CRUD-API/models"
 	"fmt"
 
 	"github.com/gin-gonic/gin"
@@ -17,59 +17,6 @@ func NewResearcherHandler(db *gorm.DB) *ResearcherHandler {
 	return &ResearcherHandler{db: db}
 }
 
-type Researcher struct {
-	ProfileID   int              `json:"profile_id"`
-	FirstName   string           `json:"first_name"`
-	LastName    string           `json:"last_name"`
-	University  string           `json:"university"`
-	AddressHome string           `json:"address_home"`
-	AddressWork string           `json:"address_work"`
-	Email       string           `json:"email"`
-	PhoneNumber string           `json:"phone_number"`
-	Degree      []tempDegree     `gorm:"-" json:"degree"`
-	Position    []tempPosition   `gorm:"-" json:"position"`
-	Program     []tempProgram    `gorm:"-" json:"program"`
-	Experience  []tempExperience `gorm:"-" json:"experience"`
-	Attach      []tempAttach     `gorm:"-" json:"attach"`
-	Explore     []tempExplore    `gorm:"-" json:"explore"`
-}
-type tempDegree struct {
-	DegreeID         int    `json:"id"`
-	DegreeType       string `json:"degree_type"`
-	DegreeProgram    string `json:"degree_program"`
-	DegreeUniversity string `json:"degree_university"`
-}
-type tempPosition struct {
-	PositionID   int    `json:"position_id"`
-	PositionName string `json:"position_name"`
-}
-type tempProgram struct {
-	ProgramID   int    `json:"program_id"`
-	ProgramName string `json:"program_name"`
-}
-
-type tempExperience struct {
-	ExperienceID         int    `json:"experience_id"`
-	ExperienceType       string `json:"experience_type"`
-	ExperienceStart      string `json:"experience_start"`
-	ExperienceEnd        string `json:"experience_end"`
-	ExperienceUniversity string `json:"experience_university"`
-	ExperienceRemark     string `json:"experience_remark"`
-}
-
-type tempAttach struct {
-	FileID      int    `json:"file_id"`
-	FileName    string `json:"file_name"`
-	FileAction  string `json:"file_action"`
-	FileStorage string `json:"file_storage"`
-}
-
-type tempExplore struct {
-	ExploreID     int    `json:"explore_id"`
-	ExploreName   string `json:"explore_name"`
-	ExploreYear   string `json:"explore_year"`
-	ExploreDetail string `json:"explore_detail"`
-}
 
 // researcher/profile_detail godoc
 // @Summary Get a Profile_detail
@@ -83,7 +30,7 @@ func (h *ResearcherHandler) ListResearcher(c *gin.Context) {
 	id := c.Param("id")
 
 	// Start getDate from Profile
-	var researcher Researcher
+	var researcher models.Researcher
 
 	// Execute the query and scan the results into the researcher struct
 	result := h.db.Raw("SELECT id as profile_id, first_name, last_name, university, address_home, address_work, email,phone_number FROM profile WHERE id = ?", id).Scan(&researcher)
@@ -96,7 +43,7 @@ func (h *ResearcherHandler) ListResearcher(c *gin.Context) {
 	}
 
 	// Start getData from Degree
-	var degrees []tempDegree
+	var degrees []models.TempDegree
 
 	degreeRows, err := h.db.Raw("SELECT id, degree_type, degree_program, degree_university FROM degree WHERE profile_id = ?", id).Rows()
 	if err != nil {
@@ -107,7 +54,7 @@ func (h *ResearcherHandler) ListResearcher(c *gin.Context) {
 	defer degreeRows.Close()
 
 	for degreeRows.Next() {
-		var degree tempDegree
+		var degree models.TempDegree
 		if err := degreeRows.Scan(&degree.DegreeID, &degree.DegreeType, &degree.DegreeProgram, &degree.DegreeUniversity); err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "An error occurred while scanning degree data"})
 			return
@@ -121,8 +68,8 @@ func (h *ResearcherHandler) ListResearcher(c *gin.Context) {
 	researcher.Degree = degrees
 
 	//start
-	// Fetch and add tempPosition data
-	var positions []tempPosition
+	// Fetch and add TempPosition data
+	var positions []models.TempPosition
 	positionRows, err := h.db.Raw("SELECT id, position_name FROM position WHERE id = ?", id).Rows()
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("An error occurred while fetching position data: %v", err.Error())})
@@ -132,7 +79,7 @@ func (h *ResearcherHandler) ListResearcher(c *gin.Context) {
 	defer positionRows.Close()
 
 	for positionRows.Next() {
-		var position tempPosition
+		var position models.TempPosition
 		if err := positionRows.Scan(&position.PositionID, &position.PositionName); err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "An error occurred while scanning position data"})
 			return
@@ -142,8 +89,8 @@ func (h *ResearcherHandler) ListResearcher(c *gin.Context) {
 	}
 	researcher.Position = positions
 
-	// Fetch and add tempProgram data
-	var programs []tempProgram
+	// Fetch and add TempProgram data
+	var programs []models.TempProgram
 	programRows, err := h.db.Raw("SELECT id, program_name FROM program WHERE id = ?", id).Rows()
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("An error occurred while fetching program data: %v", err.Error())})
@@ -153,7 +100,7 @@ func (h *ResearcherHandler) ListResearcher(c *gin.Context) {
 	defer programRows.Close()
 
 	for programRows.Next() {
-		var program tempProgram
+		var program models.TempProgram
 		if err := programRows.Scan(&program.ProgramID, &program.ProgramName); err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "An error occurred while scanning program data"})
 			return
@@ -163,8 +110,8 @@ func (h *ResearcherHandler) ListResearcher(c *gin.Context) {
 	}
 	researcher.Program = programs
 
-	// Fetch and add tempExperience data
-	var experiences []tempExperience
+	// Fetch and add TempExperience data
+	var experiences []models.TempExperience
 	experienceRows, err := h.db.Raw("SELECT id, experience_type, experience_start, experience_end, experience_university, experience_remark FROM experience WHERE id = ?", id).Rows()
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("An error occurred while fetching experience data: %v", err.Error())})
@@ -174,7 +121,7 @@ func (h *ResearcherHandler) ListResearcher(c *gin.Context) {
 	defer experienceRows.Close()
 
 	for experienceRows.Next() {
-		var experience tempExperience
+		var experience models.TempExperience
 		if err := experienceRows.Scan(&experience.ExperienceID, &experience.ExperienceType, &experience.ExperienceStart, &experience.ExperienceEnd, &experience.ExperienceUniversity, &experience.ExperienceRemark); err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "An error occurred while scanning experience data"})
 			return
@@ -184,8 +131,8 @@ func (h *ResearcherHandler) ListResearcher(c *gin.Context) {
 	}
 	researcher.Experience = experiences
 
-	// Fetch and add tempAttach data
-	var attaches []tempAttach
+	// Fetch and add TempAttach data
+	var attaches []models.TempAttach
 	attachRows, err := h.db.Raw("SELECT id, file_name, file_action, file_storage FROM profile_attach WHERE id = ?", id).Rows()
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("An error occurred while fetching attach data: %v", err.Error())})
@@ -195,7 +142,7 @@ func (h *ResearcherHandler) ListResearcher(c *gin.Context) {
 	defer attachRows.Close()
 
 	for attachRows.Next() {
-		var attach tempAttach
+		var attach models.TempAttach
 		if err := attachRows.Scan(&attach.FileID, &attach.FileName, &attach.FileAction, &attach.FileStorage); err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "An error occurred while scanning attach data"})
 			return
@@ -205,8 +152,8 @@ func (h *ResearcherHandler) ListResearcher(c *gin.Context) {
 	}
 	researcher.Attach = attaches
 
-	// Fetch and add tempExplore data
-	var explores []tempExplore
+	// Fetch and add TempExplore data
+	var explores []models.TempExplore
 	exploreRows, err := h.db.Raw("SELECT id, explore_name, explore_year, explore_detail FROM exploration WHERE id = ?", id).Rows()
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("An error occurred while fetching explore data: %v", err.Error())})
@@ -216,7 +163,7 @@ func (h *ResearcherHandler) ListResearcher(c *gin.Context) {
 	defer exploreRows.Close()
 
 	for exploreRows.Next() {
-		var explore tempExplore
+		var explore models.TempExplore
 		if err := exploreRows.Scan(&explore.ExploreID, &explore.ExploreName, &explore.ExploreYear, &explore.ExploreDetail); err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "An error occurred while scanning explore data"})
 			return
