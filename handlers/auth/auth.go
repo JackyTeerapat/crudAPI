269e58eb86cb374ee037fa9cb648add84b7692f7
songklod <songklod_d@mfec.co.th>
2023-03-31 14:55:39 +0700

    update auth

--- handlers/auth/auth.go
+++ handlers/auth/auth.go
@@ -1,10 +1,13 @@
 package auth
 
 import (
+	"CRUD-API/models"
 	"fmt"
 	"net/http"
+	"os"
 	"time"
 
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 
 	"github.com/gin-gonic/gin"
@@ -15,16 +18,70 @@ type AuthHandler struct {
 	db *gorm.DB
 }
 
-var hmacSampleSecret = []byte("3HqC0s7224Ai0XTnPBhURakeGjPaPNcUWOtrz9N+hpQ=")
+var secretKey = []byte(os.Getenv("JWT_SECRET"))
 
 func NewAuthHandler(db *gorm.DB) *AuthHandler {
 	return &AuthHandler{db: db}
 }
 
+func (u *AuthHandler) SignUp(c *gin.Context) {
+	var body struct {
+		Username string
+		Password string
+		Role     string
+	}
+
+	if c.Bind(&body) != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid body"})
+		return
+	}
+
+	// Hash password
+	hash, err := bcrypt.GenerateFromPassword([]byte(body.Password), 10)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "failed to hash password"})
+		return
+	}
+
+	// Create user
+	user := models.Register{Username: body.Username, Password: string(hash), Role: body.Role}
+	res := u.db.Create(&user)
+
+	if res.Error != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "failed to create user"})
+		return
+	}
+
+	//Response
+	c.JSON(http.StatusOK, gin.H{"message": "success"})
+}
+
 func (u *AuthHandler) Login(c *gin.Context) {
+	var body struct {
+		Username string
+		Password string
+		Role     string
+	}
+
+	if c.Bind(&body) != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid body"})
+		return
+	}
+	var user models.Register
+	u.db.First(&user, "username = ?", body.Username)
+	if user.ID == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid username or password"})
+		return
+	}
+
+	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(body.Password))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid username or password"})
+		return
+	}
 
 	token, err := GenerateToken()
-
 	if err != nil {
 		c.AbortWithStatus(http.StatusInternalServerError)
 		return
@@ -33,37 +90,16 @@ func (u *AuthHandler) Login(c *gin.Context) {
 	c.JSON(
 		http.StatusOK,
 		gin.H{
-			"token": token,
+			"status":      http.StatusOK,
+			"description": "SUCCESS",
+			"data": gin.H{
+				"user_id":  user.ID,
+				"username": user.Username,
+				"role":     user.Role,
+				"token":    token,
+			},
 		},
 	)
-
-	// var user models.User
-	// var userLogin models.User
-	// if err := c.ShouldBindJSON(&userLogin); err != nil {
-	// 	c.JSON(http.StatusBadRequest, gin.H{
-	// 		"error": err.Error(),
-	// 	})
-	// 	return
-	// }
-	// r := u.db.Table("users").Where("username = ?", userLogin.Username).First(&user)
-	// if r.RowsAffected == 0 {
-	// 	c.JSON(http.StatusNotFound, gin.H{"error": "user not found"})
-	// 	return
-	// }
-	// if err := r.Error; err != nil {
-	// 	c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-	// 	return
-	// }
-	// if userLogin.Password != user.Password {
-	// 	c.JSON(http.StatusUnauthorized, gin.H{"error": "invalid password"})
-	// 	return
-	// }
-	// token, err := GenerateToken()
-	// if err != nil {
-	// 	c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-	// 	return
-	// }
-	// c.JSON(http.StatusOK, gin.H{"token": token})
 }
 
 func (u *AuthHandler) GetUser(c *gin.Context) {
@@ -75,16 +111,17 @@ func (u *AuthHandler) GetUser(c *gin.Context) {
 	)
 }
 func GenerateToken() (tokenString string, err error) {
-
-	// Create a new token object, specifying signing method and the claims
-	// you would like it to contain.
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
 		ExpiresAt: jwt.NewNumericDate(time.Now().Add(1 * time.Minute)),
 		Issuer:    "test",
 	})
 
 	// Sign and get the complete encoded token as a string using the secret
-	tokenString, err = token.SignedString(hmacSampleSecret)
+	tokenString, err = token.SignedString(secretKey)
+
+	if err != nil {
+		return "fail to create token", err
+	}
 
 	return
 }
@@ -98,7 +135,7 @@ func ValidateToken(tokenString string) (token *jwt.Token, err error) {
 		}
 
 		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
-		return hmacSampleSecret, nil
+		return secretKey, nil
 	})
 
 	return
---
+++ initializers/connectDb.go
@@ -0,0 +1,22 @@
+package initializers
+
+import (
+	"CRUD-API/models"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func ConnectDb() *gorm.DB {
+	var err error
+	dsn := os.Getenv("DNS")
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	db.AutoMigrate(&models.User{})
+	return db
+}
