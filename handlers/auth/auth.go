4dac89618f9e4edc56b2031615b7d34e1a958b11
songklod <songklod_d@mfec.co.th>
2023-04-04 14:12:27 +0700

    update auth, create base model respone

--- handlers/auth/auth.go
+++ handlers/auth/auth.go
@@ -1,13 +1,15 @@
 package auth
 
 import (
+	"CRUD-API/handlers/api"
 	"CRUD-API/models"
 	"fmt"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 
-	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 
 	"github.com/gin-gonic/gin"
@@ -25,95 +27,94 @@ func NewAuthHandler(db *gorm.DB) *AuthHandler {
 }
 
 func (u *AuthHandler) SignUp(c *gin.Context) {
-	var body struct {
-		Username string
-		Password string
-		Role     string
+	var body models.Register
+
+	if err := c.ShouldBindJSON(&body); err != nil {
+		res := api.ResponseApi(http.StatusBadRequest, nil, fmt.Errorf("invalid body"))
+		c.JSON(http.StatusBadRequest, res)
+		return
 	}
 
-	if c.Bind(&body) != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid body"})
+	//check username is exist
+	var userExist models.User
+	u.db.Where("username = ?", body.Username).First(&userExist)
+	if userExist.ID > 0 {
+		res := api.ResponseApi(http.StatusBadRequest, nil, fmt.Errorf("username is exist"))
+		c.JSON(http.StatusBadRequest, res)
 		return
 	}
 
 	// Hash password
-	hash, err := bcrypt.GenerateFromPassword([]byte(body.Password), 10)
+	// hash, err := bcrypt.GenerateFromPassword([]byte(body.Password), 10)
 
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "failed to hash password"})
-		return
-	}
-
+	// if err != nil {
+	// 	res := api.ResponseApi(http.StatusBadRequest, nil, fmt.Errorf("failed to hash password"))
+	// 	c.JSON(http.StatusBadRequest, res)
+	// 	return
+	// }
 	// Create user
-	user := models.Register{Username: body.Username, Password: string(hash), Role: body.Role}
-	res := u.db.Create(&user)
-
-	if res.Error != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "failed to create user"})
+	user := models.User{Username: body.Username, Role: strings.ToUpper(body.Role)}
+	data := u.db.Create(&user)
+	if data.Error != nil {
+		res := api.ResponseApi(http.StatusBadRequest, nil, data.Error)
+		c.JSON(http.StatusBadRequest, res)
 		return
 	}
 
 	//Response
-	c.JSON(http.StatusOK, gin.H{"message": "success"})
+	response := models.Register{
+		ID:       user.ID,
+		Username: user.Username,
+		Role:     user.Role,
+	}
+	res := api.ResponseApi(http.StatusCreated, response, nil)
+	c.JSON(http.StatusCreated, res)
 }
 
 func (u *AuthHandler) Login(c *gin.Context) {
 	var body struct {
 		Username string
-		Password string
-		Role     string
 	}
 
-	if c.Bind(&body) != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid body"})
+	if err := c.ShouldBindJSON(&body); err != nil {
+		res := api.ResponseApi(http.StatusBadRequest, nil, fmt.Errorf("invalid body"))
+		c.JSON(http.StatusBadRequest, res)
 		return
 	}
-	var user models.Register
+	var user models.User
 	u.db.First(&user, "username = ?", body.Username)
 	if user.ID == 0 {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid username or password"})
+		res := api.ResponseApi(http.StatusBadRequest, nil, fmt.Errorf("user not found"))
+		c.JSON(http.StatusBadRequest, res)
 		return
 	}
 
-	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(body.Password))
+	// err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(body.Password))
+	// if err != nil {
+	// 	c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid username or password"})
+	// 	return
+	// }
+
+	token, err := GenerateToken(user.ID, user.Username)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid username or password"})
+		res := api.ResponseApi(http.StatusBadGateway, nil, err)
+		c.JSON(http.StatusBadGateway, res)
 		return
 	}
-
-	token, err := GenerateToken()
-	if err != nil {
-		c.AbortWithStatus(http.StatusInternalServerError)
-		return
+	data := models.LoginRespones{
+		ID:       user.ID,
+		Username: user.Username,
+		Role:     user.Role,
+		Token:    token,
 	}
-
-	c.JSON(
-		http.StatusOK,
-		gin.H{
-			"status":      http.StatusOK,
-			"description": "SUCCESS",
-			"data": gin.H{
-				"user_id":  user.ID,
-				"username": user.Username,
-				"role":     user.Role,
-				"token":    token,
-			},
-		},
-	)
+	res := api.ResponseApi(http.StatusOK, data, nil)
+	c.JSON(http.StatusOK, res)
 }
-
-func (u *AuthHandler) GetUser(c *gin.Context) {
-	c.JSON(
-		http.StatusOK,
-		gin.H{
-			"userId": c.Param("id"),
-		},
-	)
-}
-func GenerateToken() (tokenString string, err error) {
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
-		ExpiresAt: jwt.NewNumericDate(time.Now().Add(1 * time.Minute)),
-		Issuer:    "test",
+func GenerateToken(id uint, username string) (tokenString string, err error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"exp":      jwt.NewNumericDate(time.Now().Add(15 * time.Minute)),
+		"username": username,
+		"id":       strconv.Itoa(int(id)),
 	})
 
 	// Sign and get the complete encoded token as a string using the secret
@@ -133,8 +134,6 @@ func ValidateToken(tokenString string) (token *jwt.Token, err error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 		}
-
-		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
 		return secretKey, nil
 	})
 
